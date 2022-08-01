bfb95c4cd4eb2aa9da4e079f5094dd90dbb6b64f
RAFAEL OLIVEIRA DE SOUZA <22102417@dsn127m1242375.cfp127aluno.local>
2022-08-01 10:33:42 -0300

    modulos extras

---
+++ modules/calculadora.js
@@ -0,0 +1,49 @@
+/**************************************************************************************
+ * Objetivo: Arquivo que contem todas as funções para calculos matemáticos
+ * Autor: Rafael Oliveira
+ * Data de Criação: 01/08/2022
+ * Versão: 1.0
+ *************************************************************************************/
+
+function calcular(valor1, valor2, operacaoMat){
+    
+
+    //Criação de variáveis local para recebimento dos atributos encaminhados na função
+    let number1 = valor1
+    let number2 = valor2
+    let operacao = operacaoMat.toUpperCase()
+    let resultado
+    let error = false
+
+    if(isNaN(number1) || isNaN(number2)){
+        console.log('Erro - Apenas números são aceitos')
+        error = true
+    }
+    else{
+        //Estrutura de decisão relacionada a escolha da operação matematica desejada
+        if(operacao == 'SOMAR' || operacao == '+')
+            resultado = number1 + number2
+        else if(operacao == 'SUBTRAIR'|| operacao == '-')
+            resultado = number1 - number2
+        else if(operacao == 'MULTIPLICAR'|| operacao == '*')
+            resultado = number1 * number2
+        else if(operacao == 'DIVIDIR'|| operacao == '/'){
+            //Tratamento de divisão por 0
+            if(number2 == 0){
+                console.log('Erro - Não é possível divisão por zero')
+                error = true
+            }
+            else
+            resultado = number1 / number2
+        }
+        else{
+            console.log('Erro - Não foi escolhida uma operação válida')
+            error = true
+        }
+    }
+    
+    if(error)
+        return false
+    else
+        return resultado
+}
